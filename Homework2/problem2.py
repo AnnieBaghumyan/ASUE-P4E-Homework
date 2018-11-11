c8a13de5cfef8e4e39118b7f7be66523d7097c79
ani <anniebaghumyan@gmail.com>
2018-11-11 14:11:23 +0400

    add Homework2

--- Homework2/problem2.py
+++ Homework2/problem2.py
@@ -3,23 +3,22 @@
 import random
 
 num = random.randint(1, 100)
-guess = input('Guess the number between 1 and 100: ')
 number_of_guesses = 0
-
-if guess == "exit":
-	print("Exit")
+guess = "0"
 
 while int(guess) != num:
+	guess = input("Guess the number between 1 and 100: ")
+	
+	if guess == 'exit':
+		print("Exit")
+		break
+
 	number_of_guesses += 1
-	if int(guess) < num: 
-		guess = input('Your guess is too low, please continue...')
-	elif int(guess) > num: 
-		guess = input('Your guess is too high, please continue...')
-	else:
-                break
 
-if int(guess) == num:
-	number_of_guesses = str(number_of_guesses)
-	print("You're right. Congratulations! The number was ", num, ". You guessed it in " + number_of_guesses + "tries.") 
+	if int(guess) < num:
+		print('Your guess is too low, please continue...')
+	elif int(guess) > num:
+		print('Your guess is too high, please continue...')
 
-
+else:
+	print('You are right. Congratulations! The number was ' + str(num) + '. You guessed it in ' + str(number_of_guesses) + ' tries.')
